8a91b3be349c72720cebc21c295e7d6b9278ae65
Tom Hind (Cox) <tom.jcox@outlook.com>
2021-08-02 21:16:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const fs = require('fs')
-
-const content = 'Some content!'
+const fs = require('fs');
+var randomWords = require('random-words');
+var randomip = require('random-ip');
+var logfilename = new Date().getTime() + 'file.log'
+const content = 'logmsg' + ',' + randomip('192.168.2.0', 24) + ',' + randomWords({ min: 3, max: 10, separator:' ' }) + ',' + randomWords({exactly:5, wordsPerString:2});
 
 try {
-  const data = fs.writeFileSync('/Users/joe/test.txt', content)
+  const data = fs.writeFileSync(logfilename, content)
   //file written successfully
 } catch (err) {
   console.error(err)
